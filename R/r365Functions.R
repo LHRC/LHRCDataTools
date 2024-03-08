fc0d388f6fa980d442ce858a748b33b7bd471e25
Martin Donovan <martindonovan3@gmail.com>
2024-03-08 15:24:20 -0500

    c

--- R/r365Functions.R
+++ R/r365Functions.R
@@ -46,6 +46,7 @@ getLocationsDF <- function(user, password){
 
 }
 
+#' @export
 getTransactionsForLocationBetweenDF <- function(locationID, startDate, endDate, user, password) {
   #"https://odata.restaurant365.net/api/v2/views/Transaction?$filter=date%20ge%202024-02-26T00:00:00Z%20and%20date%20lt%202024-02-29T00:00:00Z"
   txEndpoint <- stringr::str_glue("https://odata.restaurant365.net/api/v2/views/Transaction?$filter=locationId%20eq%20{locationID}%20and%20date%20ge%20{startDate}T00:00:00Z%20and%20date%20lt%20{endDate}T23:59:59Z")
